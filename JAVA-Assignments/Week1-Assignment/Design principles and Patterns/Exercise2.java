dc900bb6c5c09d4a3d2cd3e2de90615049a44ed9
Hanumanthu yeswanth <yeswanthhanumanthu1687@gmail.com>
2025-06-22 11:18:11 +0530

    Update Exercise2.java

--- JAVA-Assignments/Week1-Assignment/Design principles and Patterns/Exercise2.java
+++ JAVA-Assignments/Week1-Assignment/Design principles and Patterns/Exercise2.java
@@ -1,1 +1,31 @@
-
+interface Document {
+    void open();
+}
+class Word implements Document {
+    public void open() { System.out.println("Opening Word"); }
+}
+class Pdf implements Document {
+    public void open() { System.out.println("Opening PDF"); }
+}
+class Excel implements Document {
+    public void open() { System.out.println("Opening Excel"); }
+}
+abstract class DocumentFactory {
+    abstract Document create();
+}
+class WordFactory extends DocumentFactory {
+    Document create() { return new Word(); }
+}
+class PdfFactory extends DocumentFactory {
+    Document create() { return new Pdf(); }
+}
+class ExcelFactory extends DocumentFactory {
+    Document create() { return new Excel(); }
+}
+public class Main {
+    public static void main(String[] args) {
+        new WordFactory().create().open();
+        new PdfFactory().create().open();
+        new ExcelFactory().create().open();
+    }
+}
