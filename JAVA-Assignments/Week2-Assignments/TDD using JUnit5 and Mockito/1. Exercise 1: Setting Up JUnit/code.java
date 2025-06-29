7c6acc9f3001b93776b60bc6fad8e1eeadac3c4a
Hanumanthu yeswanth <yeswanthhanumanthu1687@gmail.com>
2025-06-29 16:20:08 +0530

    Update code.java

--- JAVA-Assignments/Week2-Assignments/TDD using JUnit5 and Mockito/1. Exercise 1: Setting Up JUnit/code.java
+++ JAVA-Assignments/Week2-Assignments/TDD using JUnit5 and Mockito/1. Exercise 1: Setting Up JUnit/code.java
@@ -1,1 +1,22 @@
+Program: Calculator.java
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+Program: JunitTestClass
+CalculatorTest.java
+import static org.junit.Assert.*;
+import org.junit.Test;
 
+public class CalculatorTest {
+
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        int result = calc.add(5, 5);
+        System.out.println("Testing Addition: 5 + 5 = " + result);
+        assertEquals(10, result);
+    }
+}
