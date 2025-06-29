d36fe84bb9c8d2f8712f7f7630a31f431c92c6a5
Hanumanthu yeswanth <yeswanthhanumanthu1687@gmail.com>
2025-06-29 16:25:20 +0530

    Update code.java

--- JAVA-Assignments/Week2-Assignments/TDD using JUnit5 and Mockito/Mockito exercises/1. Exercise 1: Mocking and Stubbing/code.java
+++ JAVA-Assignments/Week2-Assignments/TDD using JUnit5 and Mockito/Mockito exercises/1. Exercise 1: Mocking and Stubbing/code.java
@@ -1,1 +1,40 @@
+ExternalApi.java
+public interface ExternalApi {
+    String getData();
+}
+MyServices.java:
+public class MyService {
+    private ExternalApi api;
 
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+}
+MyServiceTest.java:
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+    
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+       
+        MyService service = new MyService(mockApi);
+
+       
+        String result = service.fetchData();
+        assertEquals("Mock Data", result);
+    }
+}
