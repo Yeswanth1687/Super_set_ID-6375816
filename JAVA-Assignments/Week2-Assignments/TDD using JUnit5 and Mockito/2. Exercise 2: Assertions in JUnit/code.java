e365ded377374cbd51e9d4dc28b99622d1c318fb
Hanumanthu yeswanth <yeswanthhanumanthu1687@gmail.com>
2025-06-29 16:21:55 +0530

    Update code.java

--- JAVA-Assignments/Week2-Assignments/TDD using JUnit5 and Mockito/2. Exercise 2: Assertions in JUnit/code.java
+++ JAVA-Assignments/Week2-Assignments/TDD using JUnit5 and Mockito/2. Exercise 2: Assertions in JUnit/code.java
@@ -1,1 +1,41 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class MyLogicTest {
+
+    MyLogic logic = new MyLogic();
+
+    @Test
+    public void testAllAssertions() {
+   
+        assertEquals(10, logic.add(7, 3));
+
+     
+        assertTrue(logic.isPositive(5));
+
+     
+        assertFalse(logic.isPositive(-1));
+
+      
+        assertNull(logic.returnNull());
+
+        
+        assertNotNull(logic.returnObject());
+
+       
+        assertEquals("JUnit", logic.echo("JUnit"));
+
+       
+        assertTrue(logic.echo("hello").startsWith("h"));
+
+        
+        assertFalse(logic.isStringEmpty("Not Empty"));
+
+       
+        Integer x = 100;
+        assertSame(x, logic.sameReference(x));
+
+        
+        assertNotSame(new String("a"), new String("a"));
+    }
+}
